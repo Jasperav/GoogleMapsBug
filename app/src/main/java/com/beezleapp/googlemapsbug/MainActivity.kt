b50c9def12a7d125d040492635199f0a4d71362c
jaspervisser <jasperav@hotmail.com>
2023-05-31 11:53:31 +0200

    first commit

--- app/src/main/java/com/beezleapp/googlemapsbug/MainActivity.kt
+++ app/src/main/java/com/beezleapp/googlemapsbug/MainActivity.kt
@@ -3,17 +3,25 @@ package com.beezleapp.googlemapsbug
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.beezleapp.googlemapsbug.ui.theme.GoogleMapsBugTheme
-import com.google.maps.android.compose.GoogleMap
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
+import com.google.maps.android.compose.*
+import com.google.maps.android.compose.clustering.Clustering
+
+val defaultLocation = LatLng(45.440140, 1.503809)
 
 class MainActivity : ComponentActivity() {
+    @OptIn(MapsComposeExperimentalApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,35 +31,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GoogleMap(cameraPositionState = CameraPositionState(
+                        position = CameraPosition.fromLatLngZoom(
+                            defaultLocation, 18f
+                        )
+                    )) {
+                        Marker(state = MarkerState(defaultLocation))
+                        Clustering(items = listOf(ClusterItemCustom())) {
+                            Box(
+                                modifier = Modifier
+                                    .requiredHeight(100.dp)
+                                    .requiredWidth(100.dp)
+                                    .background(
+                                        Color.Blue.copy(alpha = 0.5f)
+                                    )
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Greeting(name: String) {
-    Scaffold(
-        topBar = {
-            TopAppBar(title = { Text("Title") })
-        }) { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
-            GoogleMap()
-            Text("Some text")
-            Text("Some text")
-            Text("Some text")
-            Text("Some text")
-            Text("Some text")
-        }
+class ClusterItemCustom : ClusterItem {
+    override fun getPosition() = defaultLocation
+    override fun getTitle(): String? {
+        return null
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GoogleMapsBugTheme {
-        Greeting("Android")
+    override fun getSnippet(): String? {
+        return null
     }
 }
